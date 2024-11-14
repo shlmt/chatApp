fd53ea76bc308e71d1fc19ddee14c28e8206ad90
shlmt <sh7618sh@gmail.com>
2024-11-14 10:35:36 +0200

    config firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCx9bNeUk8HJd4RzZtkzp0399Xd4XKxnqI",
+    authDomain: "ng-chat-1f814.firebaseapp.com",
+    projectId: "ng-chat-1f814",
+    storageBucket: "ng-chat-1f814.firebasestorage.app",
+    messagingSenderId: "941156842101",
+    appId: "1:941156842101:web:7cab562e65fe2ae6f0563c",
+    measurementId: "G-GELWP3YZMP"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyCx9bNeUk8HJd4RzZtkzp0399Xd4XKxnqI",
+    authDomain: "ng-chat-1f814.firebaseapp.com",
+    projectId: "ng-chat-1f814",
+    storageBucket: "ng-chat-1f814.firebasestorage.app",
+    messagingSenderId: "941156842101",
+    appId: "1:941156842101:web:7cab562e65fe2ae6f0563c",
+    measurementId: "G-GELWP3YZMP"
+  }
 };
 
 /*
