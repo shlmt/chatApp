83824cf7ed2b027c234b757d13d6706b18416993
shlmt <sh7618sh@gmail.com>
2024-11-14 13:13:09 +0200

    routing & authGuard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { ChatContainerComponent } from './components/chat-container/chat-container.component';
+import { AuthGuard } from './guards/auth.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'chat', component: ChatContainerComponent, canActivate: [AuthGuard] },
+  { path: 'chat/:roomId', component: ChatContainerComponent, canActivate: [AuthGuard] },
+  { path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/guards/auth.guard.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+import { tap } from 'rxjs/operators'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+  constructor(private authService: AuthService, private router: Router) { }
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean> {
+    return this.authService.isLoggedIn().pipe(tap(loggedIn=>{
+      if(!loggedIn) this.router.navigate(['/'])
+    })
+    );
+  }
+  
+}
