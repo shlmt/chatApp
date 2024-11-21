d8f899003b257bb471573f19a044b9268cc0f233
shlmt <sh7618sh@gmail.com>
2024-11-21 14:17:08 +0200

    some styling & fixes

--- src/app/components/room-list/room-list.component.ts
+++ src/app/components/room-list/room-list.component.ts
@@ -1,15 +1,25 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ChatRoom } from '../../models';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-room-list',
   templateUrl: './room-list.component.html',
   styleUrls: ['./room-list.component.scss']
 })
-export class RoomListComponent {
+export class RoomListComponent implements OnInit {
   
   @Input() rooms:ChatRoom[] = []
 
-  constructor() { }
+  public currentRoomId=''
+
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(params => {
+      this.currentRoomId = params.get('roomId') || ''
+    });
+  }
+
 
 }
