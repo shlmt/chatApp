967ff0815d27682e977b0398352fbceca157d856
shlmt <sh7618sh@gmail.com>
2025-01-07 18:15:09 +0200

    fixes emoji

--- src/app/components/emoji-keyboard/emoji-keyboard.component.ts
+++ src/app/components/emoji-keyboard/emoji-keyboard.component.ts
@@ -6,16 +6,17 @@ import { Component, EventEmitter, HostListener, Output } from '@angular/core';
   styleUrls: ['./emoji-keyboard.component.scss']
 })
 export class EmojiKeyboardComponent {
+  keyboardId = `keyboard-${Math.random().toString(36).substr(2, 9)}`
   emojis: string[] = ['😊', '😂', '😍', '🙃', '🥳', '🤔', '😢', '❤️', '👍', '🎉']
   isOpen: boolean = false
 
   @Output() emojiSelected = new EventEmitter<string>()
 
   @HostListener('document:click', ['$event']) onClickOutside(event: Event): void {
-    const target = event.target as HTMLElement;
-    if (!target.closest('.emoji-keyboard')) {
-      this.isOpen = false;
-    }
+    const target = event.target as HTMLElement
+    const clickedKeyboardId = target.closest('.emoji-keyboard')?.getAttribute('data-id')
+    if (clickedKeyboardId !== this.keyboardId) 
+      this.isOpen = false
   }
 
   toggleKeyboard(): void {
